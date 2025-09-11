ced2ecf491dcdee7c481f2b46213f5f988cb03cb
Balagur Dmitry <farnitin@yandex.ru>
2025-09-12 02:33:04 +0400

    add StatMapper

--- common-mappers/src/main/java/ru/practicum/mappers/StatMapper.java
+++ common-mappers/src/main/java/ru/practicum/mappers/StatMapper.java
@@ -1,4 +1,16 @@
 package ru.practicum.mappers;
 
-public class StatMapper {
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import ru.practicum.explorewithme.Stat;
+import ru.practicum.stats.StatRequestDto;
+import ru.practicum.stats.StatResponseDto;
+
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface StatMapper {
+
+    Stat toStat(StatRequestDto statRequestDto);
+
+    StatResponseDto toStatResponseDto(Stat stat);
 }
