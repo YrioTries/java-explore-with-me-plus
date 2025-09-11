ca9f7b5fb593f30b5318d73d0948adc022d3c1eb
Your Name <your.email@example.com>
2025-09-12 01:50:23 +0500

    настроил docker-compose добавил StatsClient и StatsClientConfig

--- statSvc/dto/src/main/java/ru/practicum/StatResponseDto.java
+++ statSvc/dto/src/main/java/ru/practicum/StatResponseDto.java
@@ -1,16 +1,4 @@
 package ru.practicum;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class StatResponseDto {
-    private String app;
-    private String url;
-    private Long count;
 }
