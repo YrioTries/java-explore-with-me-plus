35ccae356395bce097983a32269469424b94effa
Your Name <your.email@example.com>
2025-09-11 22:55:53 +0500

    исправил Dockerfile и docker-compose.yml

--- statSvc/server/src/main/java/ru/practicum/explorewithme/StatService.java
+++ statSvc/server/src/main/java/ru/practicum/explorewithme/StatService.java
@@ -7,8 +7,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatService {
+
     void createStat(HttpServletRequest request);
-    
+
     List<StatResponseDto> getStats(LocalDateTime start, LocalDateTime end,
                                    List<String> uris, Boolean unique);
 }
