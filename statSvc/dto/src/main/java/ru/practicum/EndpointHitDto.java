4c0fbbb122d263b514e9b3746f4658000a4ff471
Your Name <your.email@example.com>
2025-09-12 02:02:01 +0500

    настроил docker-compose добавил StatsClient и StatsClientConfig

--- statSvc/dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ statSvc/dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -23,5 +24,6 @@ public class EndpointHitDto {
     private String ip;
 
     @NotNull(message = "Timestamp cannot be null")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
