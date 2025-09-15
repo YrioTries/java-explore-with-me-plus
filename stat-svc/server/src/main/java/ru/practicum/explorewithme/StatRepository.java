bf782649c4687754125b001c0ab9b8870ae5311a
Balagur Dmitry <farnitin@yandex.ru>
2025-09-15 17:11:37 +0400

    delete annotation Repository in StatRepository

--- stat-svc/server/src/main/java/ru/practicum/explorewithme/StatRepository.java
+++ stat-svc/server/src/main/java/ru/practicum/explorewithme/StatRepository.java
@@ -4,12 +4,10 @@ import ru.practicum.StatResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface StatRepository extends JpaRepository<Stat, Long> {
 
     @Query("SELECT new ru.practicum.StatResponseDto(s.app, s.uri, COUNT(s)) " +
