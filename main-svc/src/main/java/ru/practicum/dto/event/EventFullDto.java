a8a7fb20444c64670b3570ea16c5e28470d5d9fc
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-09-19 02:40:06 +0300

    Реализовать dto согласно спецификации

---
+++ main-svc/src/main/java/ru/practicum/dto/event/EventFullDto.java
@@ -0,0 +1,53 @@
+package ru.practicum.dto.event;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.dto.category.CategoryDto;
+import ru.practicum.dto.user.UserShortDto;
+import ru.practicum.enums.State;
+import ru.practicum.dto.Location;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventFullDto {
+    private Long id;
+
+    private String annotation;
+
+    private CategoryDto category;
+
+    private Integer confirmedRequests;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdOn;
+
+    private LocalDateTime publishedOn;
+
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+
+    private UserShortDto initiator;
+
+    private Location location;
+
+    private Boolean paid;
+
+    private int participantLimit;
+
+    private State state;
+
+    private Boolean requestModeration;
+
+    private String title;
+
+    private Long views;
+}
