8bc99b73bbf043bc4831b6bbcd8ecc015ae78efb
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-09-21 23:51:27 +0300

    Реализовать сервис CompilationServiceImpl с двумя пустыми методами

---
+++ main-svc/src/main/java/ru/practicum/service/compilation/CompilationServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.practicum.service.compilation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.dto.compilation.CompilationDto;
+import ru.practicum.repository.CompilationRepository;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CompilationServiceImpl implements CompilationService {
+    private final CompilationRepository compilationRepository;
+
+    @Override
+    public List<CompilationDto> getAllCompilations(Boolean pinned, Integer from, Integer size) {
+        return List.of();
+    }
+
+    @Override
+    public CompilationDto getCompilationById(Long compId) {
+        return null;
+    }
+}
