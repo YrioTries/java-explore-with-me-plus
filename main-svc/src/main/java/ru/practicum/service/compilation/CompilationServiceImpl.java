3d0933efe9340f271a8200a1d8b89515cce5e6d7
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-09-22 00:18:46 +0300

    Реализовать в сервисе CompilationServiceImpl метод getAllCompilations по выгрузке подборок, найденных согласно параметрам фильтрации

--- main-svc/src/main/java/ru/practicum/service/compilation/CompilationServiceImpl.java
+++ main-svc/src/main/java/ru/practicum/service/compilation/CompilationServiceImpl.java
@@ -2,6 +2,7 @@ package ru.practicum.service.compilation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.dto.compilation.CompilationDto;
 import ru.practicum.mapper.CompilationMapper;
@@ -18,11 +19,26 @@ public class CompilationServiceImpl implements CompilationService {
 
     @Override
     public List<CompilationDto> getAllCompilations(Boolean pinned, Integer from, Integer size) {
-        return List.of();
+        log.info("Сервис получил запрос на получение списка подборок");
+        PageRequest pageRequest = PageRequest.of(from, size);
+        if (pinned != null) {
+            log.info("Получаем из БД подборки, у которых поле pinned={}", pinned);
+            return compilationRepository.findAllByPinned(pinned, pageRequest)
+                    .stream()
+                    .map(compilationMapper::toCompilationDto)
+                    .toList();
+        } else {
+            log.info("Получаем из БД все подборки");
+            return compilationRepository.findAll(pageRequest)
+                    .stream()
+                    .map(compilationMapper::toCompilationDto)
+                    .toList();
+        }
     }
 
     @Override
     public CompilationDto getCompilationById(Long compId) {
+        log.info("");
         return null;
     }
 }
