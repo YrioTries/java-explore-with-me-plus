9bb5240c72d3778b45bbda632c536f771a29aaa0
Your Name <your.email@example.com>
2025-09-24 15:09:23 +0500

    исправил pom.xml модуля и application.yaml

--- main-svc/src/main/java/ru/practicum/exceptions/GlobalExceptionHandler.java
+++ main-svc/src/main/java/ru/practicum/exceptions/GlobalExceptionHandler.java
@@ -14,21 +14,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleNotFoundException(NotFoundException e) {
-        return new ApiError(List.of(), e.getMessage(), "The required object was not found.",
+        return new ApiError(List.of(), e.getMessage(), "Искомый объект не был найден.",
                 HttpStatus.NOT_FOUND, LocalDateTime.now());
     }
 
     @ExceptionHandler(ConflictException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConflictException(ConflictException e) {
-        return new ApiError(List.of(), e.getMessage(), "For the requested operation the conditions are not met.",
+        return new ApiError(List.of(), e.getMessage(), "Для запрошенной операции условия не выполнены.",
                 HttpStatus.CONFLICT, LocalDateTime.now());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleException(Exception e) {
-        return new ApiError(List.of(), e.getMessage(), "Internal server error.",
+        return new ApiError(List.of(), e.getMessage(), "Внутренняя ошибка сервера.",
                 HttpStatus.INTERNAL_SERVER_ERROR, LocalDateTime.now());
     }
 }
--- main-svc/src/main/java/ru/practicum/mapper/CompilationMapper.java
+++ main-svc/src/main/java/ru/practicum/mapper/CompilationMapper.java
@@ -1,10 +1,25 @@
 package ru.practicum.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.dto.compilation.CompilationDto;
+import ru.practicum.dto.event.EventShortDto;
 import ru.practicum.model.Compilation;
+import ru.practicum.model.Event;
 
-@Mapper(componentModel = "spring", uses = EventMapper.class)
+import java.util.HashSet;
+import java.util.Set;
+
+@Mapper(componentModel = "spring")
 public interface CompilationMapper {
+
+    @Mapping(target = "events", expression = "java(mapEvents(compilation.getEvents()))")
     CompilationDto toCompilationDto(Compilation compilation);
+
+    default Set<EventShortDto> mapEvents(Set<Event> events) {
+        if (events == null) {
+            return new HashSet<>();
+        }
+        return new HashSet<>();
+    }
 }
