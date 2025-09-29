084a1160896ab6590b20d6b5611b72ea4fd67687
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-09-29 22:28:49 +0300

    Исправить код согласно логам github actions

--- main-svc/src/main/java/ru/practicum/explorewithme/service/event/PublicEventServiceImpl.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/service/event/PublicEventServiceImpl.java
@@ -1,22 +1,29 @@
 package ru.practicum.explorewithme.service.event;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.EndpointHitDto;
+import ru.practicum.StatResponseDto;
+import ru.practicum.StatsClient;
 import ru.practicum.explorewithme.dto.event.EventFullDto;
 import ru.practicum.explorewithme.dto.event.EventShortDto;
 import ru.practicum.explorewithme.enums.EventState;
+import ru.practicum.explorewithme.exception.ConflictException;
 import ru.practicum.explorewithme.exception.NotFoundException;
 import ru.practicum.explorewithme.mapper.EventMapper;
 import ru.practicum.explorewithme.model.Event;
 import ru.practicum.explorewithme.repository.EventRepository;
 import ru.practicum.explorewithme.repository.ParticipationRequestRepository;
-import ru.practicum.explorewithme.service.StatsService;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,54 +32,122 @@ public class PublicEventServiceImpl implements PublicEventService {
     private final EventRepository eventRepository;
     private final EventMapper eventMapper;
     private final ParticipationRequestRepository participationRequestRepository;
-    private final StatsService statsService;
+
+    private final StatsClient statsClient;
 
     @Transactional(readOnly = true)
     @Override
     public List<EventShortDto> getEventsPublic(String text, List<Long> categories, Boolean paid,
                                                LocalDateTime rangeStart, LocalDateTime rangeEnd,
-                                               Boolean onlyAvailable, String sort, Pageable pageable) {
-        if (rangeStart == null) rangeStart = LocalDateTime.now();
-        if (rangeEnd == null) rangeEnd = LocalDateTime.now().plusYears(100);
-
-        List<Event> events = eventRepository.findEventsPublic(text, categories, paid, rangeStart, rangeEnd, pageable)
-                .getContent();
-
-        if (Boolean.TRUE.equals(onlyAvailable)) {
-            events = events.stream()
-                    .filter(event -> {
-                        Long confirmedRequests = participationRequestRepository.countConfirmedRequestsByEventId(event.getId());
-                        return event.getParticipantLimit() == 0 || confirmedRequests < event.getParticipantLimit();
-                    })
-                    .collect(Collectors.toList());
-        }
-
-        if ("VIEWS".equals(sort)) {
-            events.sort((e1, e2) -> Long.compare(e2.getViews(), e1.getViews()));
-        } else {
-            events.sort((e1, e2) -> e2.getEventDate().compareTo(e1.getEventDate()));
-        }
-
-        return events.stream()
+                                               Boolean onlyAvailable, String sort, Pageable pageable,
+                                               HttpServletRequest request) {
+        verifyRange(rangeStart, rangeEnd);
+        statsClient.hit(EndpointHitDto
+                .builder()
+                .app("ewm-service")
+                .uri(request.getRequestURI())
+                .ip(request.getRemoteAddr())
+                .timestamp(LocalDateTime.now())
+                .build());
+        Specification<Event> spec = Specification.where(null);
+        if (text != null && !text.isBlank()) spec = spec.and(searchText(text.toLowerCase()));
+        if (categories != null && !categories.isEmpty()) spec = spec.and(searchCategoryIn(categories));
+        spec = spec.and(searchAfterDate(rangeStart));
+        if (rangeEnd != null) spec = spec.and(searchBeforeDate(rangeEnd));
+        if (onlyAvailable) spec = spec.and(searchAvailable());
+        spec = spec.and(searchPublished());
+        List<Event> results = eventRepository.findAll(spec, pageable).toList();
+        Map<Long, Long> views = getEventsViews(results);
+        return results
+                .stream()
                 .map(eventMapper::toEventShortDto)
-                .collect(Collectors.toList());
+                .peek(dto -> dto.setViews(views.getOrDefault(dto.getId(), 0L)))
+                .toList();
     }
 
     @Transactional
     @Override
-    public EventFullDto getEventById(Long id) {
+    public EventFullDto getEventById(Long id, HttpServletRequest request) {
         Event event = eventRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Event not found"));
-
         if (event.getState() != EventState.PUBLISHED) {
             throw new NotFoundException("Event not found");
         }
+        statsClient.hit(EndpointHitDto
+                .builder()
+                .app("ewm-service")
+                .uri(request.getRequestURI())
+                .ip(request.getRemoteAddr())
+                .timestamp(LocalDateTime.now())
+                .build());
+        EventFullDto eventDto = eventMapper.toEventFullDto(event);
+        Map<Long, Long> views = getEventsViews(List.of(event));
+        eventDto.setViews(views.getOrDefault(id, 0L));
+        return eventDto;
+    }
 
-        event.setViews(event.getViews() + 1);
-        Event updatedEvent = eventRepository.save(event);
+    private Map<Long, Long> getEventsViews(List<Event> events) {
+        List<String> uris = events
+                .stream()
+                .map(event -> String.format("/events/%s", event.getId()))
+                .toList();
+        LocalDateTime startDate = events
+                .stream()
+                .map(Event::getCreatedOn)
+                .min(LocalDateTime::compareTo)
+                .orElse(null);
+        Map<Long, Long> viewStats = new HashMap<>();
+        if (startDate != null) {
+            List<StatResponseDto> stats = statsClient.getStats(startDate, LocalDateTime.now(),
+                    uris, true);
+            viewStats = stats
+                    .stream()
+                    .filter(s -> s.getUri().startsWith("/events/"))
+                    .collect(Collectors.toMap(s -> Long.parseLong(s.getUri().substring("/events/".length())),
+                            StatResponseDto::getHits));
+        }
+        return viewStats;
+    }
 
-        statsService.recordEventView(id);
+    private void verifyRange(LocalDateTime rangeStart, LocalDateTime rangeEnd) {
+        if (rangeStart != null && rangeEnd != null) {
+            if (rangeStart.isAfter(rangeEnd)) {
+                throw new ConflictException("Дата начала ивента не может быть позже даты окончания");
+            }
+        }
+    }
 
-        return eventMapper.toEventFullDto(updatedEvent);
+    private Specification<Event> searchText(String text) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.or(
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("annotation")), "%" + text + "%"),
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), "%" + text + "%")
+                );
+    }
+
+    private Specification<Event> searchCategoryIn(List<Long> categories) {
+        return (root, query, criteriaBuilder) ->
+                root.get("category").get("id").in(categories);
+    }
+
+    private Specification<Event> searchAfterDate(LocalDateTime rangeStart) {
+        LocalDateTime start = Objects.requireNonNullElse(rangeStart, LocalDateTime.now());
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.greaterThan(root.get("eventDate"), start);
+    }
+
+    private Specification<Event> searchBeforeDate(LocalDateTime rangeEnd) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.lessThan(root.get("eventDate"), rangeEnd);
+    }
+
+    private Specification<Event> searchAvailable() {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("participantLimit"), 0);
+    }
+
+    private Specification<Event> searchPublished() {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("state"), EventState.PUBLISHED);
     }
 }
