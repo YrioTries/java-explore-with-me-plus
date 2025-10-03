e998da8e33ca95bb6434f694ce7e588161fd3ee1
Balagur Dmitry <farnitin@yandex.ru>
2025-10-04 00:44:14 +0400

    add annotations Positive and Validated to Private part

--- main-svc/src/main/java/ru/practicum/explorewithme/controller/privates/PrivateEventController.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/controller/privates/PrivateEventController.java
@@ -1,8 +1,10 @@
 package ru.practicum.explorewithme.controller.privates;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.dto.event.EventFullDto;
 import ru.practicum.explorewithme.dto.event.EventShortDto;
@@ -13,6 +15,7 @@ import ru.practicum.explorewithme.service.event.PrivateEventService;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/users/{userId}/events")
 @RequiredArgsConstructor
@@ -20,7 +23,7 @@ public class PrivateEventController {
     private final PrivateEventService privateEventService;
 
     @GetMapping
-    public List<EventShortDto> getEventsByUser(@PathVariable Long userId,
+    public List<EventShortDto> getEventsByUser(@PathVariable @Positive Long userId,
                                                @RequestParam(defaultValue = "0") int from,
                                                @RequestParam(defaultValue = "10") int size) {
         return privateEventService.getEventsByUser(userId, PageRequest.of(from / size, size));
@@ -28,20 +31,20 @@ public class PrivateEventController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public EventFullDto createEvent(@PathVariable Long userId,
+    public EventFullDto createEvent(@PathVariable @Positive Long userId,
                                     @RequestBody @Valid NewEventDto newEventDto) {
         return privateEventService.createEvent(userId, newEventDto);
     }
 
     @GetMapping("/{eventId}")
-    public EventFullDto getEventByUser(@PathVariable Long userId,
-                                       @PathVariable Long eventId) {
+    public EventFullDto getEventByUser(@PathVariable @Positive Long userId,
+                                       @PathVariable @Positive Long eventId) {
         return privateEventService.getEventByUser(userId, eventId);
     }
 
     @PatchMapping("/{eventId}")
-    public EventFullDto updateEventByUser(@PathVariable Long userId,
-                                          @PathVariable Long eventId,
+    public EventFullDto updateEventByUser(@PathVariable @Positive Long userId,
+                                          @PathVariable @Positive Long eventId,
                                           @RequestBody @Valid UpdateEventUserRequest updateRequest) {
         return privateEventService.updateEventByUser(userId, eventId, updateRequest);
     }
--- main-svc/src/main/java/ru/practicum/explorewithme/controller/privates/PrivateRequestController.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/controller/privates/PrivateRequestController.java
@@ -1,7 +1,9 @@
 package ru.practicum.explorewithme.controller.privates;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.dto.request.EventRequestStatusUpdateRequest;
 import ru.practicum.explorewithme.dto.request.EventRequestStatusUpdateResult;
@@ -11,6 +13,7 @@ import ru.practicum.explorewithme.service.request.RequestService;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/users/{userId}")
 @RequiredArgsConstructor
@@ -18,33 +21,33 @@ public class PrivateRequestController {
     private final RequestService participationRequestService;
 
     @GetMapping("/events/{eventId}/requests")
-    public List<ParticipationRequestDto> getEventRequests(@PathVariable Long userId,
-                                                          @PathVariable Long eventId) {
+    public List<ParticipationRequestDto> getEventRequests(@PathVariable @Positive Long userId,
+                                                          @PathVariable @Positive Long eventId) {
         return participationRequestService.getEventRequests(userId, eventId);
     }
 
     @PatchMapping("/events/{eventId}/requests")
-    public EventRequestStatusUpdateResult updateRequestStatus(@PathVariable Long userId,
-                                                              @PathVariable Long eventId,
+    public EventRequestStatusUpdateResult updateRequestStatus(@PathVariable @Positive Long userId,
+                                                              @PathVariable @Positive Long eventId,
                                                               @RequestBody @Valid EventRequestStatusUpdateRequest updateRequest) {
         return participationRequestService.updateRequestStatus(userId, eventId, updateRequest);
     }
 
     @GetMapping("/requests")
-    public List<ParticipationRequestDto> getUserRequests(@PathVariable Long userId) {
+    public List<ParticipationRequestDto> getUserRequests(@PathVariable @Positive Long userId) {
         return participationRequestService.getUserRequests(userId);
     }
 
     @PostMapping("/requests")
     @ResponseStatus(HttpStatus.CREATED)
-    public ParticipationRequestDto createRequest(@PathVariable Long userId,
-                                                 @RequestParam Long eventId) {
+    public ParticipationRequestDto createRequest(@PathVariable @Positive Long userId,
+                                                 @RequestParam @Positive Long eventId) {
         return participationRequestService.createRequest(userId, eventId);
     }
 
     @PatchMapping("/requests/{requestId}/cancel")
-    public ParticipationRequestDto cancelRequest(@PathVariable Long userId,
-                                                 @PathVariable Long requestId) {
+    public ParticipationRequestDto cancelRequest(@PathVariable @Positive Long userId,
+                                                 @PathVariable @Positive Long requestId) {
         return participationRequestService.cancelRequest(userId, requestId);
     }
 }
