7b5d5a6fbd45918c81874e8c33e9a193f6be9f34
Balagur Dmitry <farnitin@yandex.ru>
2025-10-04 01:50:27 +0400

    fix style issues again

--- main-svc/src/main/java/ru/practicum/explorewithme/repository/ParticipationRequestRepository.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/repository/ParticipationRequestRepository.java
@@ -28,6 +28,7 @@ public interface ParticipationRequestRepository extends JpaRepository<Participat
     Long countConfirmedRequestsByEventId(@Param("eventId") Long eventId);
 
     List<ParticipationRequest> findByEventIdAndStatus(Long eventId, RequestStatus status);
+
     List<ParticipationRequest> findAllByEventIdInAndStatus(List<Long> eventIds, RequestStatus status);
 
     @Modifying
