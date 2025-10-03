80900a6078eaf50853e1ff1259bdb1467838d21b
Balagur Dmitry <farnitin@yandex.ru>
2025-10-04 01:48:32 +0400

    fix style issues

--- main-svc/src/main/java/ru/practicum/explorewithme/repository/ParticipationRequestRepository.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/repository/ParticipationRequestRepository.java
@@ -13,10 +13,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ParticipationRequestRepository extends JpaRepository<ParticipationRequest, Long> {
+
     List<ParticipationRequest> findByRequesterId(Long requesterId);
+
     List<ParticipationRequest> findByEventId(Long eventId);
+
     Optional<ParticipationRequest> findByEventIdAndRequesterId(Long eventId, Long requesterId);
+
     Optional<ParticipationRequest> findByEventAndRequester(Event event, User requester);
+
     List<ParticipationRequest> findByIdIn(List<Long> requestIds);
 
     @Query("SELECT COUNT(pr) FROM ParticipationRequest pr WHERE pr.event.id = :eventId AND pr.status = 'CONFIRMED'")
--- main-svc/src/main/java/ru/practicum/explorewithme/service/request/ParticipationRequestServiceImpl.java
+++ main-svc/src/main/java/ru/practicum/explorewithme/service/request/ParticipationRequestServiceImpl.java
@@ -21,7 +21,6 @@ import ru.practicum.explorewithme.repository.UserRepository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
